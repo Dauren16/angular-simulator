c0d83367f30a193c6a3008a80d2139cee2352422
Даурен <dauren@MacBook-Pro-Dauren.local>
2026-06-17 05:16:46 +0500

    Fixed format

--- src/app/pipes/plural.pipe.ts
+++ src/app/pipes/plural.pipe.ts
@@ -4,15 +4,18 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'plural',
 })
 export class PluralPipe implements PipeTransform {
+
   transform(value: number | string, singular: string, few: string, many: string): string {
     const count: number = typeof value === 'string' ? Number(value) : value;
     if (Number.isNaN(count)) {
       return '';
     }
+    
     const remainder: number = count % 100;
     const ending: number = remainder >= 11 && remainder <= 14 ? remainder : remainder % 10;
-    if (ending === 1) return `${count} ${singular}`;
-    if (ending >= 2 && ending <= 4) return `${count} ${few}`;
-    return `${count} ${many}`;
+    if (ending === 1) return `${ count } ${ singular }`;
+    if (ending >= 2 && ending <= 4) return `${ count } ${ few }`;
+    return `${ count } ${ many }`;
   }
+
 }
